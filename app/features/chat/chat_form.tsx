8a82ea2e380dfb687697acedce4ff1cc41f1d768
Yujiluigi <yujisato.usk.jpn@gmail.com>
2026-02-25 07:46:04 +0100

    refactor: optimize chat message processing and enhance error handling
    - Improved the structure of message processing functions for better readability and maintainability.
    - Enhanced error handling mechanisms to provide clearer feedback during message transmission.
    - Updated the integration of server-sent events to ensure more reliable message updates in the chat interface.

--- app/features/chat/chat_form.tsx
+++ app/features/chat/chat_form.tsx
@@ -1,39 +1,15 @@
 import React from "react";
-import { stream_SSE } from "../../lib/stream_sse";
-import { api_endpoints } from "../../maps/api_endpoints";
 import { useChat } from "../../stores/chat_store/chat_store";
 
 export const ChatForm = () => {
-  const { setStreamingMessage, streamingMessage, sendMessage } = useChat();
+  const { sendMessage } = useChat();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     try {
       e.preventDefault();
       sendMessage(e.currentTarget.message.value);
       e.currentTarget.reset();
-      // setReasoningState({ status: "reasoning", summary: "" });
-      // await new Promise((resolve) => setTimeout(resolve, 1000));
-      // setReasoningState({
-      //   status: "completed",
-      //   summary: "reasoning completed",
-      // });
-      return;
-      const form = e.currentTarget;
-      const formData = new FormData(form);
-      const message = formData.get("message") as string;
-      let chunkTexts = "";
-      stream_SSE(api_endpoints.chat.openai.root, { message }, async (ev) => {
-        if (ev.type === "reasoning_summary_delta") {
-          console.log("...reasoning");
-        }
-        if (ev.type === "text_delta") {
-          chunkTexts += ev.data?.delta;
 
-          setStreamingMessage({
-            ...streamingMessage,
-            content: chunkTexts,
-          });
-        }
-      });
+      return;
     } catch (error) {
       console.error(error);
     }
